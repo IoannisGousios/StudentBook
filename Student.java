ab86873832f37e75da56f5b970a19db614e46ade
IoannisGousios <gousiosjohn@gmail.com>
2018-10-27 16:17:21 +0300

    improved some code

--- Student.java
+++ Student.java
@@ -1,12 +1,12 @@
 public class Student{		
-	public String firstName;
-	public String lastName;
-	public String age;
-	public String height;
-	public String tuition;
-	public String date;
-	public String phone;
-	public String conduct;
+	private String firstName;
+	private String lastName;
+	private String age;
+	private String height;
+	private String tuition;
+	private String date;
+	private String phone;
+	private String conduct;
 
 	public Student(String firstName, String lastName, String age, String height, String tuition,String date, String phone) {
 		this.firstName = firstName;
@@ -24,67 +24,41 @@ public class Student{
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
 	public String getLastName() {
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
+	
 	public String getAge() {
 		return age;
 	}
 
-	public void setAge(String age) {
-		this.age = age;
-	}
-
+	
 	public String getHeight() {
 		return height;
 	}
 
-	public void setHeight(String height) {
-		this.height = height;
-	}
-
+	
 	public String getTuition() {
 		return tuition;
 	}
 
-	public void setTuition(String tuition) {
-		this.tuition = tuition;
-	}
-
+	
 	public String getDate() {
 		return date;
 	}
 
-	public void setDate(String date) {
-		this.date = date;
-	}
-
+	
 	public String getPhone() {
 		return phone;
 	}
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
+	
 	public String getConduct() {
 		return conduct;
 	}
 
-	public void setConduct(String conduct) {
-		this.conduct = conduct;
-	}
-
-
+	
 	@Override
 	public String toString() {
 		return "==============================================================================================================="
